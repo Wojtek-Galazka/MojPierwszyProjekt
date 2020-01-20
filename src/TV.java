730513b8fd15283f0fef57084978c9a06857cc02
Lenovo <wojciech.galazka.info@gmail.com>
2020-01-20 19:21:29 +0100

    Add TV class

---
+++ src/TV.java
@@ -0,0 +1,9 @@
+public class TV {
+    String marka;
+    int rozdzielczosc;
+
+    public TV(String marka, int rozdzielczosc) {
+        this.marka = marka;
+        this.rozdzielczosc = rozdzielczosc;
+    }
+}
